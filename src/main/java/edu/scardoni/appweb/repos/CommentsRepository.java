61da28d6ff93fed0df404c10857056f294fc24de
Francesco <virgolabonomofs@gmail.com>
2020-05-18 10:54:19 +0200

    microblog 2

--- src/main/java/edu/scardoni/appweb/repos/CommentsRepository.java
+++ src/main/java/edu/scardoni/appweb/repos/CommentsRepository.java
@@ -7,6 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
+
+
+
+
+
 @Repository
 public interface CommentsRepository extends JpaRepository<CommentBlog, Long> {
 
